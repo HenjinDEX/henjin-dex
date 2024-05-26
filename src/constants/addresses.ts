40748929221dd1f54f457741adf6c0585077d4a3
Nekojita <nekojita@henjin.xyz>
2024-05-26 10:41:25 +0700

    chore: hekla release 1.0.2

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -2,10 +2,10 @@ import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
     [ChainId.Katla]: '0xfEE449019c51242F3b4E96A0bc66B455950c2650',
-    [ChainId.Hekla]: '0x6F2B40BD3Cf527b0d82BaE0A826B05A9df7991d4'
+    [ChainId.Hekla]: '0xb0756a80B7404e13fc21DA142D100B786fAc968e'
 }
 
 export const POOL_INIT_CODE_HASH = {
     [ChainId.Katla]: '0x177d5fbf994f4d130c008797563306f1a168dc689f81b2fa23b4396931014d91',
-    [ChainId.Hekla]: '0xf96d2474815c32e070cd63233f06af5413efc5dcb430aee4ff18cc29007c562d'
+    [ChainId.Hekla]: '0x4b9e4a8044ce5695e06fce9421a63b6f5c3db8a561eebb30ea4c775469e36eaf'
 }
