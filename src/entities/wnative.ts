675c7b37d08adb5d05c4b28a7f57f575f8a416aa
Nekojita <nekojita@henjin.xyz>
2024-05-27 22:35:20 +0700

    feat: add weth mainnet

--- src/entities/wnative.ts
+++ src/entities/wnative.ts
@@ -18,5 +18,12 @@ export const WNATIVE: { [chainId: number]: Token } = {
     18,
     'WETH',
     'WRAPPED ETH'
+  ),
+  [ChainId.Taiko]: new Token(
+    ChainId.Taiko,
+    '0xA51894664A773981C6C112C43ce576f315d5b1B6',
+    18,
+    'WETH',
+    'WRAPPED ETH'
   )
 };
