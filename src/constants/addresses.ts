f80149bbb5fafc601fca6a72f1acd9ef75595e10
Nekojita <nekojita@henjin.xyz>
2025-01-09 05:50:51 +0100

    feat: change base testnet

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -4,7 +4,7 @@ export const POOL_DEPLOYER_ADDRESSES = {
   [ChainId.Katla]: '0xfEE449019c51242F3b4E96A0bc66B455950c2650',
   [ChainId.Hekla]: '0x7C6C4cACed640f7a06cD32272d9D7262916498Ac',
   [ChainId.Taiko]: '0x0d22b434E478386Cd3564956BFc722073B3508f6',
-  [ChainId.BaseSepolia]: '0x0d22b434E478386Cd3564956BFc722073B3508f6',
+  [ChainId.BaseSepolia]: '0xA0fBf4Af9113329EE5B6638AcfBA9c30d9a99208',
 };
 
 export const POOL_INIT_CODE_HASH = {
@@ -15,5 +15,5 @@ export const POOL_INIT_CODE_HASH = {
   [ChainId.Taiko]:
     '0x4b9e4a8044ce5695e06fce9421a63b6f5c3db8a561eebb30ea4c775469e36eaf',
   [ChainId.BaseSepolia]:
-    '0xb3fc09be5eb433d99b1ec89fd8435aaf5ffea75c1879e19028aa2414a14b3c85',
+    '0x4b9e4a8044ce5695e06fce9421a63b6f5c3db8a561eebb30ea4c775469e36eaf',
 };
